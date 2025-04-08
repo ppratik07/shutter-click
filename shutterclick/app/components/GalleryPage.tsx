791286009ecd900713618321edf29abaca4d5e64
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-08 21:26:37 +0530

    Gallery page

--- shutterclick/app/components/GalleryPage.tsx
+++ shutterclick/app/components/GalleryPage.tsx
@@ -1,13 +1,21 @@
 import PhotoGallery from "./PhotoGallery";
 
 
-const imageUrls = [
+const roundimageUrls = [
+    "https://images.unsplash.com/photo-1618397806877-f0187730803f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1676923901465-14903a3fcc68?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1743065272129-5e261bad0c46?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1663954862772-a29434a37222?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D",
+];
+const squareimageUrls = [
     "https://images.unsplash.com/photo-1618397806877-f0187730803f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "/images/img2.jpg",
     "/images/img3.jpg",
     "/images/img4.jpg",
-];
+]
 
-export default function GalleryPage() {
-    return <PhotoGallery images={imageUrls} />;
+export default function GalleryPage({ variant }: { variant: 'rectangle' | 'square' }) {
+    return <div>
+        <PhotoGallery images={variant === 'rectangle' ? roundimageUrls : squareimageUrls} variant={variant} />
+    </div>
 }
