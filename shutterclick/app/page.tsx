500f99b2df021760eb24d2374fa2753f35ce2e78
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-08 21:56:40 +0530

    LandingPage

--- shutterclick/app/page.tsx
+++ shutterclick/app/page.tsx
@@ -1,36 +1,9 @@
-import CardGrids from "./components/CardGrids";
-import ClientCenteredBanner from "./components/ClientCenteredBanner";
-import Footer from "./components/Footer";
-import GalleryPage from "./components/GalleryPage";
-import Hero from "./components/HeroSection";
-import Promo from "./components/Promo";
-import ReadyToWork from "./components/ReadyToWork";
+import { LandingPage } from "./pages/LandingPage";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <ClientCenteredBanner
-        title="Capturing the Essence of Divine Beauty & Memorable Moments"
-      />
-      <CardGrids />
-      <ClientCenteredBanner
-        title="CURRENT SPECIALS"
-        buttonText="CLICK HERE"
-      />
-      <GalleryPage variant="rectangle" />
-      <ClientCenteredBanner
-        title="Other Package Offers"
-      />
-      <Promo />
-      <ReadyToWork />
-      <GalleryPage variant="square" />
-      <div className="w-full h-full flex justify-center items-center text-center">
-        <h4 className="text-xs tracking-widest text-black uppercase">
-          follow me on instagram<br />@shutterclicks
-        </h4>
-      </div>
-      <Footer/>
+      <LandingPage/>
     </main>
   );
 }
