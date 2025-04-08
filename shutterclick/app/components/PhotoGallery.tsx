828ee37d0681e0dbcf94fc05b031799bbd66e016
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-08 21:26:55 +0530

    photo gallery

--- shutterclick/app/components/PhotoGallery.tsx
+++ shutterclick/app/components/PhotoGallery.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 
 interface PhotoGalleryProps {
-  images: string[];
+    images: string[];
+    variant: "rectangle" | "square";
 }
 
-export default function PhotoGallery({ images }: PhotoGalleryProps) {
-  return (
-    <section className="w-full flex justify-center px-4 md:px-16 py-12 bg-white">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-        {images.map((src, index) => (
-          <img
-            key={index}
-            src={src}
-            alt={`Gallery Image ${index + 1}`}
-            className="w-full h-auto object-cover rounded-md shadow-md"
-          />
-        ))}
-      </div>
-    </section>
-  );
+export default function PhotoGallery({ images, variant }: PhotoGalleryProps) {
+    return (
+        <section className="w-full flex justify-center px-4 md:px-16 py-12 bg-white">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                {images.map((src, index) => (
+                    <img
+                        key={index}
+                        src={src}
+                        alt={`Gallery Image ${index + 1}`}
+                        className={`object-cover shadow-md ${variant === "rectangle"
+                                ? "w-full h-auto"
+                                : "w-full aspect-square"
+                            }`}
+                    />
+                ))}
+            </div>
+        </section>
+    )
 }
