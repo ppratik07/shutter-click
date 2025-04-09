91787b77ed302e9f87d9ae6d2921ac67b181d8f5
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-09 11:57:55 +0530

    created page.tsx

---
+++ shutterclick/app/booking/page.tsx
@@ -0,0 +1,11 @@
+import MiniSessionGrid from "../components/MiniSessionGrid";
+import WelcomeSection from "../components/WelcomeSection";
+
+export default function BookingPage() {
+    return (
+        <main>
+            <WelcomeSection />
+            <MiniSessionGrid />
+        </main>
+    );
+}
