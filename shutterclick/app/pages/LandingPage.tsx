6a5c2418c4d82d984b8deb434faa4063e3be1d3b
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-09 11:30:17 +0530

    updating img

--- shutterclick/app/pages/LandingPage.tsx
+++ shutterclick/app/pages/LandingPage.tsx
@@ -23,7 +23,8 @@ export const LandingPage = () => {
                 title="Other Package Offers"
             />
             <Promo />
-            <ReadyToWork />
+            <ReadyToWork title1="Ready to Work With Me" title2="Let’s connect and make some magic"
+             imageUrl="https://images.unsplash.com/photo-1438962136829-452260720431?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
             <GalleryPage variant="square" />
             <div className="w-full h-full flex justify-center items-center text-center">
                 <h4 className="text-xs tracking-widest text-black uppercase">
